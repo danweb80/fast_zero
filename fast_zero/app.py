88cdbbb146d7a3087ca30828c504e426cf66f254
Daniel Weber <prof.daniel.weber@gmail.com>
2024-07-07 16:16:56 -0300

    versão com ERRO ao criar db sqlite

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
-from fast_zero.schemas import UserSchema, UserPublic
 from http import HTTPStatus
+
+from fastapi import FastAPI
+
+from fast_zero.schemas import UserPublic, UserSchema
 
 app = FastAPI()
 
+
 @app.get('/')
 def read_root():
-    return 'Coelhinho peludo!'
+    return 'é nóis!'
+
 
 @app.post('/users/', response_model=UserPublic, status_code=HTTPStatus.CREATED)
 def create_user(user: UserSchema):
+    # faz as validações (consulta no banco de o username e email já existem)
+    # casso não existirem salva no banco e retorna OK
     return user
---
+++ tests/test_db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_zero.models import User, table_registry
+
+
+def test_create_user():
+    engine = create_engine('sqlite:///database.db')
+
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        user = User(username='junin', email='junin@mail.com', password='123')
+
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+    assert user.id == 1
